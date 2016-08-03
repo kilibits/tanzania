d460f32b0a6b3a1b6faf2f22cacb92c09d9a543d
Damas <dam6660@rit.edu>
2016-08-03 00:52:52 -0700

    More structures to match db schemas

--- api.go
+++ api.go
@@ -1,16 +1,41 @@
 package main
 
 type Profile struct {
-	Name         string
-	Party        string
-	Constituency string
-	Phone        string
-	Email        string
-	ImageURL     string
-	MemberType   string
-	Address      string
-	BirthDate    time
-	ID           int16
+	Name                string
+	Party               string
+	Constituency        string
+	Phone               string
+	Email               string
+	ImageURL            string
+	MemberType          string
+	Address             string
+	BirthDate           time
+	ID                  int16
+	PoliticalExperience []PolHist
+	EducationHistory    []EduHist
+	EmploymentHistory   []EmpHist
+}
+
+type EduHist struct {
+	Institution string
+	Level       string
+	Award       string
+	From        int8
+	To          int8
+}
+
+type EmpHist struct {
+	Institution string
+	Position    string
+	From        int8
+	To          int8
+}
+
+type PolHist struct {
+	Institution string
+	Position    string
+	From        int8
+	To          int8
 }
 
 func getProfile() {
