baa5ef0dfbcd9cd24f89a93121d539e8c3ca870b
Damas <dam6660@rit.edu>
2016-08-03 00:45:12 -0700

    Initial work on rest API, included db in repo

---
+++ api.go
@@ -0,0 +1,34 @@
+package main
+
+type Profile struct {
+	Name         string
+	Party        string
+	Constituency string
+	Phone        string
+	Email        string
+	ImageURL     string
+	MemberType   string
+	Address      string
+	BirthDate    time
+	ID           int16
+}
+
+func getProfile() {
+
+}
+
+func getAllProfiles() {
+
+}
+
+func getEmploymentHistory() {
+
+}
+
+func getEducationHistory() {
+
+}
+
+func getPoliticalExperienceHistory() {
+
+}
