bd649d91335d2947cbc542aa47c69e6ba252307c
Damas <dam6660@rit.edu>
2016-08-16 16:57:38 -0700

    code clean up on api file

--- api.go
+++ api.go
@@ -2,35 +2,33 @@ package main
 
 import (
 	"database/sql"
-	"io"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
 	"gopkg.in/gorp.v1"
 )
 
+//A Profile of member of parliament with basic details
 type Profile struct {
-	Name        string
-	Group       string
-	Area        string
-	Phone       string
-	Email       string
-	Image       string
-	Member_Type string
-	Address     string
-	Birth_Date  string
-	Id          int
-	Term        int
-	Source      string
+	Name       string
+	Group      string
+	Area       string
+	Phone      string
+	Email      string
+	Image      string
+	MemberType string `db:"member_type"`
+	Address    string
+	BirthDate  string `db:"birth_date"`
+	Id         int
+	Term       int
+	Source     string
 	// PoliticalExperience []PolHist
 	// EducationHistory    []EduHist
 	// EmploymentHistory   []EmpHist
 }
 
+//EduHist (Education History) of member of parliament
 type EduHist struct {
 	Institution string
 	Level       string
@@ -39,6 +37,7 @@ type EduHist struct {
 	To          int
 }
 
+//EmpHist (Employment History) of member of parliament
 type EmpHist struct {
 	Institution string
 	Position    string
@@ -46,6 +45,7 @@ type EmpHist struct {
 	To          int
 }
 
+//PolHist (Political Experience History) of member of parliament
 type PolHist struct {
 	Institution string
 	Position    string
@@ -75,7 +75,7 @@ func dbInit() *gorp.DbMap {
 	return dbMap
 }
 
-func getProfiles(c *gin.Context) {
+func getAllProfiles(c *gin.Context) {
 
 	var profiles []Profile
 	_, err := dbMap.Select(&profiles, "SELECT * FROM profile")
@@ -92,7 +92,7 @@ func getProfiles(c *gin.Context) {
 func getEducationHistory(c *gin.Context) {
 	var edu []EduHist
 	name := c.Params.ByName("id")
-	_, err := dbMap.Select(&edu, "SELECT schoolName, level, award, [from], [to] FROM profile JOIN education_history ON swdata.id = education_history.mp_id WHERE swdata.name LIKE '%?%'", name)
+	_, err := dbMap.Select(&edu, "SELECT institution, level, award, [from], [to] FROM profile JOIN education_history ON swdata.id = education_history.mp_id WHERE swdata.name LIKE '%?%'", name)
 
 	if err != nil {
 		log.Fatalf("Select statement failed -> %v", err.Error())
@@ -104,10 +104,9 @@ func getEducationHistory(c *gin.Context) {
 func main() {
 	dbMap = dbInit()
 	app := gin.Default()
-	app.GET("/profiles", getProfiles)
+	app.GET("/profiles", getAllProfiles)
 	app.GET("/profiles/:profile_id", getProfile)
 	app.GET("/education/:id", getEducationHistory)
-	app.GET("/download", downloadImages)
 
 	app.Run(":9000")
 }
@@ -116,34 +115,34 @@ func main() {
 //Stored images will then be stored on a CDN instead of calling from parliament site
 
 //TODO: Add http request retries to avoid timeouts
-func downloadImages(c *gin.Context) {
-
-	var links []Profile
-	_, err := dbMap.Select(&links, "SELECT id, image FROM profile")
-
-	if err != nil {
-		log.Fatalf("Select statement failed -> %v", err.Error())
-	}
-
-	for _, prof := range links {
-		var _, err = os.Stat("./images/" + strconv.Itoa(prof.Id) + ".jpeg")
-
-		if os.IsNotExist(err) {
-			file, err := os.Create("./images/" + strconv.Itoa(prof.Id) + ".jpeg")
-			if err != nil {
-				log.Fatalf("Failed to create image -> %v", err.Error())
-			}
-			defer file.Close()
-
-			response, err := http.Get(prof.Image)
-
-			if err != nil {
-				log.Fatalf("Unable to get url -> %v", err.Error())
-			}
-
-			defer response.Body.Close()
-
-			io.Copy(file, response.Body)
-		}
-	}
-}
+// func downloadImages(c *gin.Context) {
+//
+// 	var links []Profile
+// 	_, err := dbMap.Select(&links, "SELECT id, image FROM profile")
+//
+// 	if err != nil {
+// 		log.Fatalf("Select statement failed -> %v", err.Error())
+// 	}
+//
+// 	for _, prof := range links {
+// 		var _, err = os.Stat("./images/" + strconv.Itoa(prof.Id) + ".jpeg")
+//
+// 		if os.IsNotExist(err) {
+// 			file, err := os.Create("./images/" + strconv.Itoa(prof.Id) + ".jpeg")
+// 			if err != nil {
+// 				log.Fatalf("Failed to create image -> %v", err.Error())
+// 			}
+// 			defer file.Close()
+//
+// 			response, err := http.Get(prof.Image)
+//
+// 			if err != nil {
+// 				log.Fatalf("Unable to get url -> %v", err.Error())
+// 			}
+//
+// 			defer response.Body.Close()
+//
+// 			io.Copy(file, response.Body)
+// 		}
+// 	}
+//}
